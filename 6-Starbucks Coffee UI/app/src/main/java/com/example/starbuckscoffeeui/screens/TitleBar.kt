c55444d8ec95162d168f505c8f0742497e2eb4f5
wakolivotes <votesdw@yahoo.com>
2022-11-27 15:27:35 +0300

    Fix TitleBar

--- 6-Starbucks Coffee UI/app/src/main/java/com/example/starbuckscoffeeui/screens/TitleBar.kt
+++ 6-Starbucks Coffee UI/app/src/main/java/com/example/starbuckscoffeeui/screens/TitleBar.kt
@@ -1,9 +1,23 @@
 package com.example.starbuckscoffeeui.screens
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.starbuckscoffeeui.R
+import java.util.Stack
 
 // Will hold all the views and composable functions
 // It will then be called in the MainActivity file
@@ -12,7 +26,49 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun TitleBar() {
-    Text(text = "Welcome to Starbucks Coffee")
+//    Text(text = "Welcome to Starbucks Coffee")
+    MaterialTheme() {
+        Row (
+            horizontalArrangement = Arrangement.SpaceBetween
+                )
+        {
+            Text(
+                text = "Starbucks Coffee",
+                style = TextStyle(
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 19.sp
+                )
+            )
+            Box(
+                modifier = Modifier
+                    .width(45.dp)
+                    .height(45.dp)
+            ){
+                Box()
+                {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_outline_shopping_basket_24) ,
+                        contentDescription = "Shopping Cart",
+                        modifier = Modifier.clip(shape = CircleShape,
+                        color = Color(red = 32, green = 151, blue = 64)
+                        )
+                    )
+                    Image(
+                        modifier = Modifier
+                            .width(22.dp)
+                            .height(22.dp)
+                        ,
+                        painter = painterResource(id = R.drawable.ic_outline_shopping_basket_24),
+                        contentDescription = "Shopping Cart"
+                    )
+
+
+            }
+
+        }
+        
+    }
+}
 }
 
 
