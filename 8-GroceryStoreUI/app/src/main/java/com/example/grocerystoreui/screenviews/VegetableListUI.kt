55e202fd5e3cda53b38450e8b6dc33d39eaea47b
wakolivotes <votesdw@yahoo.com>
2022-12-03 13:43:03 +0300

    Grocery UI

--- 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/VegetableListUI.kt
+++ 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/VegetableListUI.kt
@@ -1,6 +1,5 @@
 package com.example.grocerystoreui.screenviews
 
-import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
