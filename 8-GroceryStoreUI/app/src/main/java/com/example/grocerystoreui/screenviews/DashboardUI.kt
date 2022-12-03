30edd5443306bcc8e7f7dbb2e723ece279f41188
wakolivotes <votesdw@yahoo.com>
2022-12-03 12:57:45 +0300

    Grocery UI

--- 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/DashboardUI.kt
+++ 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/DashboardUI.kt
@@ -1,9 +1,94 @@
 package com.example.grocerystoreui.screenviews
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import com.example.grocerystoreui.navigation.ScreenRoutes
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.grocerystoreui.navigation.MainActions
+import com.example.grocerystoreui.R
 
 @Composable
-fun DashBoardUI(){
+fun DashBoardUI(
+    actions: MainActions,
+) {
+    val sectionState = remember {
+        mutableStateOf(DashboardSection.Home)
+    }
+    val navItems = DashboardSection.values().toList()
+
+
+    Scaffold(
+        bottomBar = {
+            items = navItems,
+            currentSection = sectionState.value,
+            onSectionSelected = {
+                sectionState.value = it
+            },
+        }
+    ) { innerPadding ->
+        val modifier = Modifier.padding(innerPadding)
+        Crossfade(
+            modifier = modifier,
+            targetState = sectionState.value
+        ) { section ->
+            when(section) {
+                DashboardSection.Home -> HomeUI(actions)
+                DashboardSection.List -> VegetableDetailUI(actions)
+                else -> {}
+            }
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(
+    items: List<DashboardSection>,
+    currentSection: DashboardSection,
+    onSectionSelected: (DashboardSection) -> UInt,
+) {
+    BottomNavigation (
+        modifier = Modifier.height(50.dp),
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = contentColorFor(MaterialTheme.colors.background)
+
+            ) {
+        items.forEach{ section ->
+            val selected = section == currentSection
+
+            val iconRes = if (selected) section.selectedIcon else section.icon
+
+            BottomNavigationItem(
+                icon = {
+                       Icon(
+                           painter = painterResource(id = iconRes),
+                           contentDescription = "Bottom nav icons",
+                           modifier = Modifier.size(24.dp)
+                       )
+                },
+                selected = selected,
+                onClick = { onSectionSelected(section) },
+                alwaysShowLabel = false
+            )
+        }
+
+    }
+}
+
+
+private enum class DashboardSection(
+    val icon: Int,
+    val selectedIcon: Int
+) {
+    Home(R.drawable.home, R.drawable.home),
+    List(R.drawable.list, R.drawable.list),
+    ShoppingCart(R.drawable.shopping_cart, R.drawable.shopping_cart),
+    Profile(R.drawable.user, R.drawable.user)
 
 }
--- 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/VegetableDetailtUI.kt
+++ 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/VegetableDetailtUI.kt
@@ -1,8 +1,155 @@
 package com.example.grocerystoreui.screenviews
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.KeyboardArrowLeft
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.grocerystoreui.R
+import com.example.grocerystoreui.navigation.MainActions
+import com.example.grocerystoreui.ui.theme.*
 
 @Composable
-fun VegetableDetailUI(){
+fun VegetableDetailUI(actions: MainActions) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(azureishWhite)
+    ) {
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(white)
+                    .padding(8.dp)
+            )
+            {
+                Icon(imageVector = Icons.Outlined.KeyboardArrowLeft,
+                    contentDescription = stringResource(R.string.text_back_icon),
+                    tint = Color.Red
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        )
+        {
+            Image(painter = painterResource(id = R.drawable.red_capsicum),
+                contentDescription = "Tomato Image",
+                contentScale = ContentScale.Crop
+            )
+        }
+        Spacer(modifier = Modifier.height(44.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(16.dp, 16.dp, 0.dp, 0.dp))
+                .background(white)
+                .padding(24.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            {
+                Text(
+                    text = "Sawi Hijau",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = "Ksh. 34.00/Kg",
+                    color = gold,
+                    fontSize = 14.sp
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Sawi Hijau It contains folate, potassium, vitamin C, and vitamin B-6 and cholesterol. Vitamin B-6 and folate prevent penampukan compounds.",
+                    color = Color.Gray,
+                    fontSize = 24.sp
+                )
+                Spacer(modifier = Modifier.height(28.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Column(modifier = Modifier.wrapContentHeight()) {
+                        Text(
+                            text = "Total",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp,
+                            color = black,
+                        )
+                        Text(
+                            text = "Ksh. 34.00/Kg",
+                            color = gold,
+                            fontSize = 14.sp
+                        )
+                        Box(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(8.dp))
+                                .background(lightSilver)
+                                .padding(4.dp),
+                            contentAlignment = Alignment.Center
+                        )
+                        {
+                            Icon(imageVector = Icons.Default.Add,
+                                contentDescription = stringResource(R.string.text_add_icon),
+                                tint = white
+                            )
+                        }
 
+                    }
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(24.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = blue
+                        ),
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Text(
+                            text = "Next",
+                            color = Color.White,
+                            fontSize = 14.sp
+                        )
+                    }
+
+                }
+            }
+        }
+    }
 }
