007b8ef971d63cde8bcebb1f086e6f95a79d924e
wakolivotes <votesdw@yahoo.com>
2022-12-04 12:23:35 +0300

    Grocery UI

--- 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/HomeUI.kt
+++ 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/HomeUI.kt
@@ -1,24 +1,28 @@
 package com.example.grocerystoreui.screenviews
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.grocerystoreui.R
 import com.example.grocerystoreui.navigation.MainActions
-import com.example.grocerystoreui.ui.theme.ghostWhite
-import com.example.grocerystoreui.ui.theme.platinum
+import com.example.grocerystoreui.ui.theme.*
 
 @Composable
 fun HomeUI(actions: MainActions) {
@@ -81,12 +85,78 @@ fun HomeUI(actions: MainActions) {
                 },
                 leadingIcon = {
                     Icon(
-                        imageVector = ,
-                        contentDescription = )
-                }
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = stringResource(R.string.text_search_icon),
+                        tint = lightSilver
+                    )
+                },
             )
+            Spacer(modifier = Modifier.height(24.dp))
+            LazyRow(
+                modifier = Modifier.height(24.dp)
+            )
+            {
+                items(itemList.size) { item ->
+                    Box(
+                        modifier = Modifier
+                            .border(
+                                color = if (item == 0) gold else Color.Transparent,
+                                width = 2.dp,
+                                shape = RoundedCornerShape(24.dp)
+                            )
+                    )
+                    {
+                        Text(
+                            modifier = Modifier
+                                .padding(
+                                    start = 16.dp,
+                                    end = 16.dp,
+                                    top = 8.dp,
+                                    bottom = 8.dp
+                                ),
+                            text = itemList[item],
+                            color = if (item == 0) gold else Color.LightGray
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(0.5f)
+                                .wrapContentHeight()
+                                .clip(RoundedCornerShape(16.dp))
+                                .background(navajoWhite)
+                        ){
+                            Column(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp))
+                            {
+                                Box(
+                                    modifier = Modifier
+                                        .clip(CircleShape)
+                                        .background(white)
+                                        .padding(4.dp)
+                                ){
+                                    Icon(
+                                        imageVector = Icons.Default.Favorite,
+                                        contentDescription = stringResource(R.string.text_heart_icon),
+                                        tint = Color.Red
+                                    )
+                                }
 
+                            }
+                        }
 
+                    }
+
+                }
+
+            }
         }
 
 
