40fc666489c743bbfddb2ca1752cd0f2090faa0c
wakolivotes <votesdw@yahoo.com>
2022-12-04 11:12:44 +0300

    Grocery UI

--- 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/HomeUI.kt
+++ 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/HomeUI.kt
@@ -1,8 +1,28 @@
 package com.example.grocerystoreui.screenviews
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.grocerystoreui.navigation.MainActions
 
 @Composable
-fun HomeUI(){
+fun HomeUI(actions: MainActions){
+    var text by remember {
+        mutableStateOf("")
+    }
+    var itemList = listOf("Favorites", "Home Sweet", "Organics", "Fresh")
+
+
+    androidx.compose.material.Surface (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            ) {
+
+
+    }
 
 }
