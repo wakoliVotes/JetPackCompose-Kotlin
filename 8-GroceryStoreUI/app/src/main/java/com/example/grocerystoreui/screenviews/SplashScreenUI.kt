dce60af4f93c09281effce75f5d35d45ce8e833d
wakolivotes <votesdw@yahoo.com>
2022-12-05 14:08:28 +0300

    Grocery UI

--- 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/SplashScreenUI.kt
+++ 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/SplashScreenUI.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -34,18 +37,9 @@ fun SplashScreenUI(actions: MainActions) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
-            Text(
-                text = "One-2-One Groceries",
-                style = TextStyle(
-                    color = MaterialTheme.colors.primary,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 16.sp
-                )
-
-
-            )
             Image(painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Logo"
+                contentDescription = "Logo",
+                modifier = Modifier.clip(CircleShape)
             )
         }
     }
