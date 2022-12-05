ee964b8e820527dacb1668036361fea79831f420
wakolivotes <votesdw@yahoo.com>
2022-12-05 14:05:13 +0300

    Grocery UI

--- 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/SplashScreenUI.kt
+++ 8-GroceryStoreUI/app/src/main/java/com/example/grocerystoreui/screenviews/SplashScreenUI.kt
@@ -6,12 +6,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.grocerystoreui.R
 import com.example.grocerystoreui.navigation.MainActions
 import kotlinx.coroutines.delay
@@ -28,6 +34,16 @@ fun SplashScreenUI(actions: MainActions) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
+            Text(
+                text = "One-2-One Groceries",
+                style = TextStyle(
+                    color = MaterialTheme.colors.primary,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+
+
+            )
             Image(painter = painterResource(id = R.drawable.logo),
                 contentDescription = "Logo"
             )
